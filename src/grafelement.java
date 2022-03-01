b34f9e21ba34c2c09e650b330e913b014c2f842e
Lukas Kolouch <lukas.kolouch@student.gyarab.cz>
2022-03-01 09:08:35 +0100

    Initial commit

---
+++ src/grafelement.java
@@ -0,0 +1,25 @@
+public class grafelement {
+
+    int xx;
+    int yy;
+    int sirka;
+    int vyska;
+
+    public grafelement(int x, int y, int sirka, int vyska) {
+        System.out.println("kons");
+        xx = x;
+        yy = y;
+        this.sirka = sirka;
+        this.vyska = vyska;
+    }
+
+    public grafelement(int x, int y) {
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+
+        grafelement ge = new grafelement(x 1, y 1, sirka 10, vyska 10);
+    }
+
+}
---
+++ src/ikona.java
@@ -0,0 +1,12 @@
+public class ikona extends grafelement{
+    public ikona(int x, int y) {
+        super(x, y);
+        System.out.println("konstruktor ikona" + xx + " " + yy);
+
+
+       /* xx = x;
+        yy = y;
+        sirka = vyska = 60;
+        */
+    }
+}
